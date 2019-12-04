c410755042e847122886d3b527436b1dab494f22
bowen163 <763893371@qq.com>
2019-12-04 11:16:50 +0800

    Initial Commit

---
+++ I am smart!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am smart!
+//
+//  Created by s20181102936 on 2019/12/4.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
